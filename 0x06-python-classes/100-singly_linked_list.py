ee514492053160a80520385a221ea0d5c150d944
Kandyriii <pmugo2002@gmail.com>
2024-01-23 07:11:04 -0800

    Singly Linked lists

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -78,4 +78,4 @@ class SinglyLinkedList:
         while tmp is not None:
             values.append(str(tmp.data))
             tmp = tmp.next_node
-        return ('\n'.join(values)
+        return ('\n'.join(values))
